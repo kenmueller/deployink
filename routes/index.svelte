d29b5ef9a43d33e2a7f4533a1c903711cf05e9d9
Ken Mueller <kenmueller0@gmail.com>
2021-11-07 12:20:42 -0800

    initial commit

---
+++ routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Deploy</title>
+</svelte:head>
+
+<main>
+	<h1>Deploy</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		height: 100%;
+	}
+
+	h1 {
+		margin: auto;
+	}
+</style>
